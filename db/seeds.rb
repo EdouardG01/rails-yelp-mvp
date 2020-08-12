faaeebae90b3e4c6a8c0b952a72ad51506fd0e4a
edouard goisbault <edouard.goisbault01@gmail.com>
2020-08-12 17:14:02 +0200

    rails yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Maestro', address: 'Nantes', phone_number: '06 54 65 78 65',category: 'french')
+Restaurant.create(name: 'Pepe', address: 'Nantes Erdre', phone_number: '06 54 78 65 09',category: 'french')
+Restaurant.create(name: 'Cotep', address: 'Nantes centre', phone_number: '06 98 54 21 34',category: 'french')
+Restaurant.create(name: 'Jojo', address: 'Ile de Nantes', phone_number: '07 24 65 77 35',category: 'french')
+Restaurant.create(name: 'Cigale', address: 'Nantes Graslin', phone_number: '02 60 87 62 95',category: 'french')
